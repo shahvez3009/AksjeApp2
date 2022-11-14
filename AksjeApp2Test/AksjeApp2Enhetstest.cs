098d9a119227c0e9a3a4e0469c065316a39383f2
ZubeyrMustafaAhmed <zubeyrahmed@hotmail.com>
2022-11-14 17:42:46 +0100

    La til enhetstest

---
+++ AksjeApp2Test/AksjeApp2Enhetstest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace AksjeApp2Test
+{
+    public class AksjeApp2Enhetstest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
+
