2bb3772fefe5a8c27d4e702dc081437a47b6ac09
Enes Ergin <enesergin1204@hotmail.com>
2022-11-14 19:26:42 +0100

    Har lagt til Backend fra prosjekt 1 , og ts filene til prosjekt 2

---
+++ AksjeApp2/DAL/AksjeRepositoryInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AksjeApp1.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace AksjeApp1.DAL
+{
+    public interface AksjeRepositoryInterface
+    {
+        Task<bool> Selg(int id, Portfolios innPortfolio);
+        Task<bool> Kjop(int id, Portfolios innPortfolio);
+        Task<Portfolio> HentEtPortfolioRad(int id);
+        Task<List<Portfolio>> HentPortfolio();
+        Task<List<Transaksjon>> HentTransaksjoner();
+        Task<Aksje> HentEnAksje(int id);
+        Task<List<Aksje>> HentAksjer();
+        Task<Bruker> HentEnBruker();
+    }
+}
---
+++ AksjeApp2/Models/Bruker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AksjeApp1.Models
+{
+    public class Bruker
+    {
+        public int Id { get; set; }
+        public string Fornavn { get; set; }
+        public string Etternavn { get; set; }
+        public int Saldo { get; set; }
+        public string Mail { get; set; }
+        public int Mobilnummer { get; set; }
+    }
+}
+
