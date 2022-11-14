15d7c77b5a6077979acbc77b1cb2b9077d449d9a
Shahvez Mahmood <72561932+shahvez3009@users.noreply.github.com>
2022-11-14 18:42:42 +0100

    navn endring fra okonomi -> hjem

---
+++ AksjeApp2/ClientApp/src/app/okonomi/hjem.ts
@@ -0,0 +1,10 @@
+﻿import { Component, OnInit } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Aksje } from "../Aksje";
+
+@Component({
+    templateUrl: "hjem.html"
+})
+
