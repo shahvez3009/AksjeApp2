3eef7b807d28e78060589f3f246f64cf2fa86bae
Shahvez Mahmood <72561932+shahvez3009@users.noreply.github.com>
2022-11-16 21:11:39 +0100

    Problemer i portfolio

--- AksjeApp2/ClientApp/src/app/portfolio/portfolio.ts
+++ AksjeApp2/ClientApp/src/app/portfolio/portfolio.ts
@@ -4,7 +4,6 @@ import { Router } from '@angular/router';
 import { PortfolioRad } from "../PortfolioRad"; 
 
 @Component({
-	//Selector: Må legge inn hvilken component den skal erstatte
 	templateUrl: "portfolio.html"
 })
 
@@ -18,15 +17,15 @@ export class Portfolio {
 	//Blir kjørt når vi kaller på denne komponenten
 	ngOnInit() {
 		this.laster = true;
-		this.hentPortfolio1();
+		this.hentPortfolio();
 	}
 
 	//S
-	hentPortfolio1() {
+	hentPortfolio() {
 		this.http.get<PortfolioRad[]>("api/portfolio/hentportfolio") 
 			.subscribe(portfolioene => {
 				console.log("Inne i portfolio")
-				this.hentPortfolio1 = portfolioene;
+				this.hentPortfolio = portfolioene;
 				this.laster = false;
 			},
 				error => console.log(error)
