83447fd3a8e6f24700ab063726174d7c7721a63d
HarmanS01 <70652644+HarmanS01@users.noreply.github.com>
2022-11-16 12:49:14 +0100

    Lagt til TS for portfolio

---
+++ AksjeApp2/ClientApp/src/app/portfolio/portfolio.ts
@@ -0,0 +1,38 @@
+﻿import { Component, OnInit } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Portfolio } from "../Portfolio"; 
+
+@Component({
+    //Selector: Må legge inn hvilken component den skal erstatte
+    templateUrl: "portfolio.html"
+})
+
+export class Portfolioo {
+    laster: boolean;
+    helePortfolio: Array<Portfolio>;
+
+    constructor(private _http: HttpClient) { }
+
+    //Blir kjørt når vi kaller på denne komponenten
+    ngOnInit() {
+        this.laster = true;
+        this.hentPortfolio();
+    }
+
+    //S
+    hentPortfolio() {
+        this._http.get<Portfolio[]>("api/portfolio/") //Skjønner ikke hva "api/portfolio betyr"
+            .subscribe(portfolioene => {
+                this.helePortfolio = portfolioene;
+                this.laster = false;
+            },
+                error => console.log(error)
+            );
+    };
+}
+
+
+
+
