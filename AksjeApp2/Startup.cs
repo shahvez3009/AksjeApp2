b78d08479d79d57c0cd03dbc4709b42c6312ae08
Shahvez Mahmood <72561932+shahvez3009@users.noreply.github.com>
2022-11-16 17:06:54 +0100

    Ryddet Startup

--- AksjeApp2/Startup.cs
+++ AksjeApp2/Startup.cs
@@ -7,13 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using AksjeApp2.DAL;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using AksjeApp2.Models;
 
 namespace AksjeApp2
