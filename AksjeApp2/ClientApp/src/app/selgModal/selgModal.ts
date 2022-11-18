6812963168ade67485f1f5814798525153e7ae21
Shahvez Mahmood <72561932+shahvez3009@users.noreply.github.com>
2022-11-18 15:46:26 +0100

    Test

--- AksjeApp2/ClientApp/src/app/selgModal/selgModal.ts
+++ AksjeApp2/ClientApp/src/app/selgModal/selgModal.ts
@@ -1,12 +1,15 @@
 ﻿import { Component } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
 	templateUrl: "selgModal.html"
 })
 
 export class SelgModal {
-
+	constructor(public modal: NgbActiveModal) { }
 }
+
 
