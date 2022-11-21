8ab05d12ccdfa73f0bf0c155425d7874f8c24b60
Enes Ergin <enesergin1204@hotmail.com>
2022-11-21 15:20:47 +0100

    kommentert ut all logging. Nå kjører det

--- AksjeApp2/Controllers/AksjeController.cs
+++ AksjeApp2/Controllers/AksjeController.cs
@@ -46,7 +46,7 @@ namespace AksjeApp2.Controllers
             if (!returOk) {
                 return BadRequest();
             }
-            _log.LogInformation("Et salg har blitt gjort på aksje med id: " + aksjeId);
+            //_log.LogInformation("Et salg har blitt gjort på aksje med id: " + aksjeId);
             return Ok();
         }
 
@@ -66,7 +66,7 @@ namespace AksjeApp2.Controllers
             {
                 return BadRequest();
             }
-			_log.LogInformation("Et kjøp har blitt gjort på aksje med id: " + aksjeId);
+			//_log.LogInformation("Et kjøp har blitt gjort på aksje med id: " + aksjeId);
             return Ok();
         }
 
@@ -85,7 +85,7 @@ namespace AksjeApp2.Controllers
             {
                 return NotFound();
             }
-            _log.LogInformation("Hentet EN bruker");
+            //_log.LogInformation("Hentet EN bruker");
             return Ok(brukeren);
         }
 
@@ -104,7 +104,7 @@ namespace AksjeApp2.Controllers
 			{
 				return NotFound();
 			}
-            _log.LogInformation("Hentet aksje med id: " + aksjeId);
+            //_log.LogInformation("Hentet aksje med id: " + aksjeId);
             return Ok(aksjen);
 		}
 
@@ -123,7 +123,7 @@ namespace AksjeApp2.Controllers
 			{
 				return NotFound();
 			}
-            _log.LogInformation("Hentet en portfoliorad med aksjeid: " + aksjeId);
+            //_log.LogInformation("Hentet en portfoliorad med aksjeid: " + aksjeId);
             return Ok(portfolioRad);
 		}
 
@@ -140,7 +140,7 @@ namespace AksjeApp2.Controllers
 			
 
 			List<Aksje> alleAksjer = await _db.HentAksjer();
-            _log.LogInformation("Akskjer har blitt hentet til 'hjem.html'");
+            //_log.LogInformation("Akskjer har blitt hentet til 'hjem.html'");
             return Ok(alleAksjer);
             
         }
@@ -156,7 +156,7 @@ namespace AksjeApp2.Controllers
 			*/
 
 			List<PortfolioRad> allePortfolio = await _db.HentPortfolio();
-            _log.LogInformation("Portfolio har blitt hentet til 'portfolio.html'");
+            //_log.LogInformation("Portfolio har blitt hentet til 'portfolio.html'");
             return Ok(allePortfolio);
 
 		}
@@ -172,7 +172,7 @@ namespace AksjeApp2.Controllers
 			*/
 
 			List<Transaksjon> alleTransaksjoner = await _db.HentTransaksjoner();
-            _log.LogInformation("Transaksjonene har blitt hentet til 'Transaksjoner.html'");
+            //_log.LogInformation("Transaksjonene har blitt hentet til 'Transaksjoner.html'");
             return Ok(alleTransaksjoner);
 		}
 
@@ -210,7 +210,7 @@ namespace AksjeApp2.Controllers
             {
                 return BadRequest();
             }
-            _log.LogInformation("En bruker har blitt lagt");
+            //_log.LogInformation("En bruker har blitt lagt");
             return Ok();
         }
 
--- AksjeApp2/Startup.cs
+++ AksjeApp2/Startup.cs
@@ -47,12 +47,12 @@ namespace AksjeApp2
         }
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
-				loggerFactory.AddFile("Logs/AksjeLog.txt");
+				//loggerFactory.AddFile("Logs/AksjeLog.txt");
                 DBInit.Seed(app);
             }
 			else
